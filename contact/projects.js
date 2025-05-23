4413bc725b1cb819553f2385513272bf6109aa5d
Michael Yang <mlyang1008@gmail.com>
2025-05-23 09:02:44 -0700

    overnight website updates

---
+++ contact/projects.js
@@ -0,0 +1,130 @@
+// Importing Project Data into the Projects Page
+// create a function in your global.js file to load project data from a JSON file and dynamically display it on the Projects page.
+// Start by defining an asynchronous function that will fetch your project data.
+export async function fetchJSON(url) {
+  // Add a check to ensure the fetch request was successful. If it wasn’t, throw an error to handle invalid responses.
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    // place this snippet inside the try block, immediately after the fetch function call.
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    // Use console.log(response) to inspect the response object in your browser’s developer tools and confirm that it’s working correctly.
+    console.log(response);
+
+    // Once you’ve verified the response is valid, parse it into a format you can work with.
+    // Here’s how to parse the response:
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching or parsing JSON data:", error);
+  }
+}
+
+//Build a renderProjects function to dynamically generate and display project content.
+// This function will allow you to reuse project rendering logic anywhere on your site.
+//Start by creating a function that accepts two parameters:
+// the project object and the containerElement where the project will be displayed.
+//Now that the basic function is ready, let’s enhance it to allow dynamic heading levels.
+// This makes the function reusable for different contexts.
+export function renderProjects(
+  projects,
+  containerElement,
+  headingLevel = "h2",
+) {
+  // write javascript that will allow dynamic heading levels based on previous function
+
+  //     Think about why you need these two parameters.
+  // CHALLENGE:
+  // What type of data should the project parameter contain?
+  // How would you test if the containerElement is valid?
+
+  //To dynamically render project details, you’ll create and populate an <article> element for each project.
+  // Before adding new project articles, ensure the container is empty to avoid duplication.
+  containerElement.innerHTML = ""; // Check if the projects array is empty
+
+  const projectCount = projects.length;
+  if (projectCount === 0) {
+    // Display a placeholder message
+    const placeholderMessage = `No projects found. Please check back later for updates.`;
+    container.innerHTML = `
+      <${headingLevel}>${placeholderMessage}</${headingLevel}>
+    `;
+  } else {
+    // Render the projects as usual
+
+    try {
+      // Select the element with the id projects-title from the DOM
+      const projectsTitleElement = document.getElementById("projects-title");
+      // Update the text content of the element
+      projectsTitleElement.textContent = `${projectCount} results`;
+    } catch {}
+    // THINK ABOUT IT:
+    // Why is it important to clear the container before adding new elements?
+    // What would happen if you skipped this step?
+
+    projects.forEach((project) => {
+      //For each project, create a new <article> element to hold its details.
+      const article = document.createElement("article");
+      //THINK ABOUT IT:
+      // Why do we use createElement instead of directly appending HTML?
+      // How does using createElement make your code more secure or modular?
+
+      //Use the innerHTML property to populate the <article> element with dynamic content.
+      article.innerHTML = `
+      
+            
+            <a href="${project["Website link"]}" target="_blank">
+              <${headingLevel}>${project["org name"]}</${headingLevel}>
+            </a>
+            <div class="wrapper">
+              <div class="description">${project.description}</div>
+              <div class="year">${project["topic / category"]}</div>
+            </div>
+            <br>
+            <img src="${project.image}" alt="${project["org name"]}" height="200" width="280" padding="10" />
+            
+        `;
+      //THINK ABOUT IT:
+      // What happens if one of the properties, like project.image, is missing?
+      // How can you handle missing or invalid data gracefully?
+
+      //Finally, append the <article> element to the provided containerElement.
+      containerElement.appendChild(article);
+    });
+
+    //Ensure containerElement is a valid DOM element in your tests.
+    if (!(containerElement instanceof Element)) {
+      throw new Error("containerElement must be a valid DOM element");
+    }
+  }
+}
+
+
+//Use the fetchJSON function to load the project data from a JSON file.
+//This code assumes your projects.json file is located in a lib folder relative to the current file.
+let projects = await fetchJSON("../json_files/orgs.json");
+//Select the container where you want to render the project articles. Use the following snippet:
+//Ensure your HTML includes a container with the class projects.
+const projectsContainer = document.querySelector(".projects");
+//Call the renderProjects function to dynamically display the fetched projects:
+//This code will render each project with an <h2> heading level.
+renderProjects(projects, projectsContainer, "h3");
+
+let selectedIndex = -1;
+
+let query = "";
+let searchInput = document.querySelector(".searchBar");
+
+searchInput.addEventListener("change", (event) => {
+  // update query value
+  query = event.target.value;
+  // filter the projects
+  let filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join("\n").toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+  // re-render legends and pie chart when event triggers
+  renderProjects(filteredProjects, projectsContainer, "h2");
+});
--- nav.js
+++ nav.js
@@ -1,94 +1,75 @@
-const PAGES = [
-  {name: 'home', url: '/'},
-  {name: 'about', url: '/about.html'},
-  {name: 'links', url: '/links.html'},
-  {name: 'gbook', url: 'http://users.smartgb.com/g/g.php?a=s&i=g18-84757-83'},
-  {name: 'hr'},
-  {name: 'journal', url: '/journal.html'},
-  {name: 'fireplace', url: '/fireplace.html'},
-  {name: 'zines', url: '/zines.html'},
-  {name: 'hr', hidden: true},
-  // {name: 'my favs', url: '/favs.html', hidden: true},
-  {name: 'my art', url: '/art.html', hidden: true},
-  {name: 'place', url: '/place.html', hidden: true},
-  {name: 'archive', url: '/archive', hidden: true},
-  {name: 'button', url: '/links.html', hidden: true},
+let pages = [
+  { url: "", title: "home" },
+  { url: "about/", title: "about" },
+  { url: "library/", title: "education library" },
+  { url: "contact/", title: "community contact portal" },
+  { url: "map/", title: "organizations map" },
+  { url: "artboard/", title: "community artboard" },
+  { url: "", title: "submit a resource" }
 ];
+// Then, create a new <nav> element (via document.createElement()) and add it inside <body> at the beginning (via element.prepend()).:
+let nav = document.createElement("nav");
+nav.className = "nav";
+nav.innerHTML = '<ul class="nav-ul nav-ul--monitor">';
+let ul = nav.querySelector("ul");
 
-const element = document.getElementById('nav');
-let inner = '';
-if (typeof(CURR_PAGE) == 'undefined') CURR_PAGE = '';
+document.body.prepend(nav);
+// Then we will use a for .. of loop to iterate over the pages on our site and add <a> elements in the <nav> for each of them. It will look like this:
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: Create link and add it to nav
+  // let’s detect whether we are running the site locally (on localhost) or on GitHub Pages,
+  // and use that to adjust the base URL for all links.
+  // We can do this by checking the current hostname and defining a constant BASE_PATH accordingly:
+  // // The location.hostname property gives us the domain of the current page.
+  // // If we’re working locally, it will be "localhost" or "127.0.0.1".
+  // // If we’re on GitHub Pages, it will be something like "yourusername.github.io".
+  const BASE_PATH =
+    location.hostname === "localhost" || location.hostname === "127.0.0.1"
+      ? "/" // Local server
+      : "/cjarscapstone/"; // GitHub Pages repo name
+  // // Then, when creating the links, we’ll check if the URL is relative (i.e. does not start with "http"),
+  // // and if so, we’ll prefix it with the BASE_PATH. This ensures that all internal links work properly both locally and when deployed.
+  // if (!url.startsWith('http')) {
+  //     url = BASE_PATH + url;
+  //   }
+  // Alternatively, we can do it more concisely using a ternary operator:
+  url = !url.startsWith("http") ? BASE_PATH + url : url;
 
-if (typeof(isMobileOrTablet) == 'undefined') {
-  isMobileOrTablet = () => {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    const isIpadPro = navigator.userAgent.match(/Mac/) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2;
-    return check || isIpadPro;
+  // Our automatically added navigation menu works, but is missing all the bells and whistles of our original one:
+  // The current page is not highlighted anymore
+  // The link to your GitHub profile does not have target="_blank" to make it open in a new tab.
+  // How can we add those back?
+  // Let’s switch to a different method of creating these links, that is more verbose, but gives us more flexibility. Instead of appending HTML strings, we will create element objects in JS and set their attributes in JS, via properties (or setAttribute() calls.
+  // So, this line of JS:
+  // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  // now becomes four lines:
+  let li = document.createElement("li");
+  li.className = "nav-li";
+  li.innerHTML = '<a class="nav-span"></a>';
+  let a = li.querySelector("a");
+  a.href = url;
+  a.textContent = title;
+  ul.append(li);
+  // We can just add a conditional to check if the link is to the current page using exactly the same check as Step 2.2 and add the class if so.
+  // All we need is to compare a.host and a.pathname to location.host and location.pathname and then use a.classList.add() to add the class.
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add("current");
+  }
+  // You can even use a.className.toggle() to do the checking and the class adding in one (very long) line!
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname,
+  );
+  // Similarly, we can add target="_blank" to external links (such as the GitHub link) by setting a.target = "_blank" to those links for which a.host is not the same as location.host.
+  // Just like class names, you can either use conditionals or do the checking and the attribute setting in one step, by using element.toggleAttribute().
+  if (a.host !== location.host) {
+    a.target = "_blank";
   }
 }
 
-function isSafari() {
-  const ua = navigator.userAgent.toLowerCase();
-  return ua.indexOf('safari') != -1 && ua.indexOf('chrome') < 0;
-}
+let footer = document.createElement("footer");
+footer.className = "nav";
+document.body.append(footer);
 
-for (let page of PAGES) {
-  const hidden = page.hasOwnProperty('hidden');
-  const wip = page.hasOwnProperty('wip');
-  if (page.name === 'hr') {
-    inner += `<hr${hidden ? ' class="hidden"' : ''}>`;
-    continue;
-  }
-  if (page.name === 'gbook') {
-    // hide the arrow in safari
-    const arrowSpan = isSafari() ? '' : '<span> ↗</span>';
-    const arrowClass = isSafari() ? '' : 'gbook--arrow';
-    inner += `<li class="nav-li gbook ${arrowClass}"><span><a class="nav-span" href="${page.url}" target="_blank">gbook</a>${arrowSpan}</span></li>`;
-    continue;
-  }
-  if (page.name === 'button') {
-    if (CURR_PAGE === 'home') {
-      // only show button on homepage
-      inner += '<a class="button hidden" href="/links.html"><img width="75" src="/img/buttons/nyanseong-wink.png"></a>';
-    }
-    continue;
-  }
-  const hiddenClass = hidden ? ' hidden' : '';
-  const wipClass = wip ? ' wip' : '';
-  const wipTooltip = wip ? ' title="wip!"' : '';
-  if (page.name === CURR_PAGE) {
-    inner += `<li class="nav-li${hiddenClass}"><span class="nav-span">→ ${page.name}</span></li>`;
-  } else {
-    inner += `<li class="nav-li${hiddenClass}"><a class="nav-span${wipClass}" href="${page.url}"${wipTooltip}>${page.name}</a></li>`;
-  }
-}
-
-// "more" button
-inner += '<li class="nav-li" id="nav-mobile-show"><a tabIndex="0" class="nav-span" id="nav-mobile-show-link">..more</a></li>';
-
-// tip text
-if (CURR_PAGE === 'zines') {
-  inner += '<li class="tip" id="esc-tip">press ESC to pause animation in background</li>';
-}
-
-// wrap in <ul>
-inner = `<ul class="nav-ul ${isMobileOrTablet() ? 'nav-ul--mobile' : 'nav-ul--monitor'}">${inner}</ul>`;
-if (element) element.innerHTML = inner;
-
-const handleClickMore = (e) => {
-  e.preventDefault();
-  document.getElementById('nav-mobile-show').style.display = 'none';
-  for (let item of document.getElementsByClassName('hidden')) {
-    item.style.display = 'block';
-  }
-  console.log(document.getElementById('nav-mobile-show'));
-};
-
-const more = document.getElementById('nav-mobile-show-link');
-more.addEventListener('click', handleClickMore);
-document.onkeydown = (e) => {
-  if (e.keyCode === 13 && document.activeElement && document.activeElement.id === 'nav-mobile-show-link') {
-    handleClickMore(e);
-  }
-};
